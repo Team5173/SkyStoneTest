61f18266c0371ee3de054cb0ee967b7e97420689
jamiha <34746242+jamiha@users.noreply.github.com>
2019-09-03 18:18:41 -0400

    Team Blue Changes
    Team Blue code should be ready to test. As well as, Team Red Code should also be ready to test.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team_Blue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team_Blue.java
@@ -6,6 +6,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.robotcore.hardware.CRServo;
 
 @TeleOp(name="Team Blue Code", group="Iterative Opmode")
 public class Team_Blue extends OpMode
@@ -19,7 +20,9 @@ public class Team_Blue extends OpMode
 
     //Servos
     private Servo clamp;
+    private CRServo arm;
 
+    double armPower;
 
     public void init() {
         telemetry.addData("Status", "Initialized");
@@ -32,6 +35,9 @@ public class Team_Blue extends OpMode
         leftDrive.setDirection(DcMotor.Direction.FORWARD);
         rightDrive.setDirection(DcMotor.Direction.REVERSE);
 
+        //SERVO names classes
+        clamp = hardwareMap.get(Servo.class,"clamp");
+        arm = hardwareMap.get(CRServo.class,"arm");
         telemetry.addData("Status", "Initialized");
     }
 
@@ -55,9 +61,20 @@ public class Team_Blue extends OpMode
 
         leftDrive.setPower(leftPower);
         rightDrive.setPower(rightPower);
+        arm.setPower(armPower);
 
-
-
+        if (gamepad1.right_stick_y > 0.1) {
+            armPower = .20;
+        } else if (gamepad1.right_stick_y < -0.1) {
+            armPower = -.20;
+        } else {
+            armPower = 0;
+        }
+        if (gamepad1.x){
+            clamp.setPosition(0.25);
+        } else if (gamepad1.b){
+            clamp.setPosition(0);
+        }
         telemetry.addData("Status", "Run Time: " + runtime.toString());
         telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
     }
